39fa8a5897320eefff5d63b8c30ba8de088bd1f3
zippyzag88 <kuriakosejain456@gmail.com>
2023-09-01 15:57:50 +0530

    intial commit + header comp

---
+++ MEMEGEN/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from "../assets/logo.png"
+
+export default function Header () {
+    return (
+        <header className="header">
+         <img src={logo} alt="logo" className="header--image" />
+         <h2 className="header--title">Meme Generator</h2>
+         <h4 className="header--project">react project</h4>
+        </header>
+    )
+}
