4b2998094fc66526561838d15c788325a5f5e684
zippyzag88 <kuriakosejain456@gmail.com>
2023-09-01 19:29:47 +0530

    memedata added

--- MEMEGEN/src/components/Meme.jsx
+++ MEMEGEN/src/components/Meme.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import memesData from "../memesData";
+
 
 export default function meme () {
+        const [memeImage, setMemeImage] = React.useState("")
+
+
+        function getMemeImage() {
+            const memesArray = memesData.data.memes
+            const randomNumber = Math.floor(Math.random() * memesArray.length)
+            setMemeImage(memesArray[randomNumber].url)
+            
+        }
+
+
+
     return (
         <main>
-            <form className="form">
-            <input type="text" 
-            className="form--inputs" placeholder="Top text" />
-            <input type="text" className="form--inputs" placeholder="Bottom text" />
-            <button className="form--button" >Get a new meme image 🖼</button>
-            </form>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--inputs"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--inputs"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
 }
